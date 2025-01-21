b5c9a580d5b9929f9baa8b18680f923ac68f7c1b
Karl <karlcampbell100@gmail.com>
2025-01-21 14:40:14 +0000

    added query selectors for all relevant html elements ready for TS logic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
 import "./../styles/styles.scss" 
+
+const currentScore = document.querySelector<HTMLParagraphElement>("#playerScore");
+const highScore = document.querySelector<HTMLParagraphElement>("#highestScore");
+const timeRemaining = document.querySelector<HTMLParagraphElement>("#timeRemaining");
+const currentQuestion = document.querySelector<HTMLParagraphElement>("#currentQuestion");
+const currentHint = document.querySelector<HTMLParagraphElement>("#promptForHint");
+const hintButton = document.querySelector<HTMLButtonElement>("#hintButton");
+const playerAnswer = document.querySelector<HTMLInputElement>("#playerInput")
+const submitButton = document.querySelector<HTMLButtonElement>("#answerSubmit")
+const startGame = document.querySelector<HTMLButtonElement>("#startGame")
+
+
+
+
+// type Questions = {
+//     question: string;
+//     answer: string;
+//     hint: string;
+//     category: string;
+
+// }
+
+
+// const questionsList = {
+//     questions: [
+//     {questionNumber: 1, promptText: "Action of moving quickly", answer: "running" }
+//     ]
+// }
